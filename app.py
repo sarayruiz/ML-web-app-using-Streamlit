609b9eb431958c6594e0451c3af6e7e55f2ec173
Saray Ruiz <sarayruizr@gmail.com>
2025-12-19 11:43:48 +0000

    avance

--- app.py
+++ app.py
@@ -11,11 +11,9 @@ st.title("Spam - Model prediction")
 st.markdown("""Power by: [Saray Ruiz](https://sarayruiz.net)""")
 st.divider()
 
-val1 = st.text_input("Introduzca URL", value="", max_chars=None, key=None, type="default", help="Introduce desde htpp...",
-                    label_visibility="visible")
+val1 = st.text_input("Introduzca URL", value="", max_chars=None, key=None, type="default", help="Introduce desde htpp...", label_visibility="visible")
     
     
-
 if st.button("Predict"):
     prediction = str(model.predict([val1])[0])
     pred_class = class_dict[prediction]
